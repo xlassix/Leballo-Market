07aa00dd6c9e0ae85d11a81aba8508fd3c178af1
xlassix <46370698+xlassix@users.noreply.github.com>
2022-02-04 06:07:39 +0100

    nftMarketPlace

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import '../styles/globals.css';
-import Nav from "../components/Nav"
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Nav></Nav>
-  )
+  <>
+    <Head>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+    <title>Leballo</title>
+  </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,22 @@
-import Head from "next/head";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import Details from "../components/MyNft";
+import dynamic from 'next/dynamic'
 import {ethers} from "ethers";
 import {useEffect,useState} from "react"
 import axios from "axios";
 import Web3Modal from "web3modal";
 
+const DynamicComponent= dynamic(() => import('../components/MyNft'), {
+  ssr: false
+})
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <title>Leballo</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+      <>
+        <Nav></Nav>
+        <DynamicComponent></DynamicComponent>
+        <Footer></Footer>
+      </>
+    );
 }
