962ea4545dc1e96e22a48e2c1cded61872e5ecb9
xlassix <46370698+xlassix@users.noreply.github.com>
2022-02-18 15:02:39 +0100

    config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftMarketPlaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftMarketPlaceAddress = "0x484728cCEC9142922FF36cc23E3aC4ca4F8eB806";
+export const nftAddress = "0xaEB2d8c27800CeF26B495409D40f776C67fE5636";
